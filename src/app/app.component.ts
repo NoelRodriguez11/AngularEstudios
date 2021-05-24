d80732c2d4c5d4e9b351cb5763f6b4e30ea93365
Noel Rodríguez Taboada <noelrodriguez2a@gmail.com>
2021-05-24 18:10:50 +0200

    Segundo dia de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'portfolio-app';
-  public cards:Array<any> = [];
+  public listdeVideos:Array<any> = [];
 
   ngOnInit(): void {
-    this.cards = [
+    this.listdeVideos = [
       {
         title:'Video 1',
-        subtitle:'Subtitle Video 1'
+        subtitle:'Subtitle Video 1',
+        img:"https://i.ytimg.com/vi/8g7yWl9NGr4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC333_lOYw9Y4cvxbDuVUCuPht5rQ"
       },
       {
         title:'Video 2',
-        subtitle:'Subtitle Video 2'
+        subtitle:'Subtitle Video 2',
+        img:"https://i.ytimg.com/vi/8g7yWl9NGr4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC333_lOYw9Y4cvxbDuVUCuPht5rQ"
       },
       {
         title:'Video 3',
-        subtitle:'Subtitle Video 3'
+        subtitle:'Subtitle Video 3',
+        img:"https://i.ytimg.com/vi/8g7yWl9NGr4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC333_lOYw9Y4cvxbDuVUCuPht5rQ"
       }
     ]
   }
